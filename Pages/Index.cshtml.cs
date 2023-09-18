a1a0734a7659b6a8b1b88cf9bb6a5fcb4d77db8c
Ali Alsaadiq <aalsaadiq@my.bcit.ca>
2023-09-18 12:16:34 -0700

    Implement lab01

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,8 @@ namespace lab01.Pages;
 
 public class IndexModel : PageModel
 {
+    [BindProperty]
+    public int? Year { get; set; }
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -12,8 +14,32 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+    public static string GetZodiac(int year)
+    {
+        string[] zodiac = new string[12];
+        zodiac[0] = "Monkey";
+        zodiac[1] = "Rooster";
+        zodiac[2] = "Dog";
+        zodiac[3] = "Pig";
+        zodiac[4] = "Rat";
+        zodiac[5] = "Ox";
+        zodiac[6] = "Tiger";
+        zodiac[7] = "Rabbit";
+        zodiac[8] = "Dragon";
+        zodiac[9] = "Snake";
+        zodiac[10] = "Horse";
+        zodiac[11] = "Goat";
+
+        int remainder = year % 12;
+        return zodiac[remainder];
+    }
+
     public void OnGet()
     {
-
     }
+
+    public void OnPost()
+    {
+    }
+
 }
